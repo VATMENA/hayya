c471d5e558c1cc887b2e1e58e4a77e53d8355374
core <core@coredoes.dev>
2024-02-13 21:39:34 -0500

    permissions debug API

--- src/lib/perms/can.ts
+++ src/lib/perms/can.ts
@@ -1,30 +1,77 @@
 import type { Role } from "@prisma/client";
 import { minimatch } from "minimatch";
 
+export interface PermissionTrace {
+  roles: Role[],
+  targetVacc: string | null,
+  userVacc: string | null,
+  req_perm: string,
+  all_permissions: string[]
+  outcome: boolean,
+  because: string,
+  checked_perms: { original: string, replaced: string, was_replaced: boolean, req_perm: string, matched: boolean }[]
+}
+
+export function _can(roles: Role[], targetVacc: string | null, userVacc: string | null, req_perm: string): [boolean, PermissionTrace] {
+  let trace: PermissionTrace = {
+    roles,
+    targetVacc,
+    userVacc,
+    req_perm,
+    all_permissions: [],
+    outcome: false,
+    because: "not yet evaluated",
+    checked_perms: []
+  };
+
+  if (roles === null || roles === undefined) {
+    trace.outcome = false;
+    trace.because = "roles is null or undefined";
+    return [false, trace];
+  }
+
+  let all_perms: string[] = [];
+
+  trace.all_permissions = all_perms;
+
+  for (let role of roles) {
+    all_perms = all_perms.concat(role.permissions);
+  }
+
+  for (let perm of all_perms) {
+    let traceperm = {
+      original: perm,
+      replaced: "",
+      req_perm,
+      matched: false,
+      was_replaced: false
+    }
+    let real_perm = perm;
+    if (targetVacc == userVacc) {
+      traceperm.was_replaced = true;
+      real_perm = perm.replace("vacc.own", `vacc.${targetVacc}`);
+    }
+    traceperm.replaced = real_perm;
+    if (minimatch(req_perm, real_perm)) {
+      traceperm.matched = true;
+      trace.checked_perms.push(traceperm);
+      return [true, trace];
+    }
+    traceperm.matched = false;
+    trace.checked_perms.push(traceperm);
+  }
+
+  trace.because = "no permissions matched";
+  return [false, trace];
+}
+
 export function can(
   roles: Role[],
   targetVacc: string | null,
   userVacc: string | null,
   req_perm: string,
 ): boolean {
-  if (roles === null || roles === undefined) {
-    return false;
-  }
-  let all_perms: string[] = [];
-  for (let role of roles) {
-    all_perms = all_perms.concat(role.permissions);
-  }
-  for (let perm of all_perms) {
-    let real_perm = perm;
-    if (targetVacc == userVacc) {
-      real_perm = perm.replace("vacc.own", `vacc.${targetVacc}`);
-    }
-    if (minimatch(req_perm, real_perm)) {
-      return true;
-    }
-  }
-
-  return false;
+  return _can(roles, targetVacc, userVacc, req_perm)[0];
 }
 
 export function canAny(
