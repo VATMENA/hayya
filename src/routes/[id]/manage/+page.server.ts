94b200b04e53a8fcf130d431d52e9b78b71d6406
core <core@coredoes.dev>
2024-03-03 09:34:49 -0500

    fix bug there

--- src/routes/[id]/manage/+page.server.ts
+++ src/routes/[id]/manage/+page.server.ts
@@ -1,9 +1,12 @@
 import type { PageServerLoad } from "./$types";
 import { can } from "$lib/perms/can";
+import { loadUserData } from "$lib/auth";
 import { EDIT_DETAILS } from "$lib/perms/permissions";
 import { redirect } from "sveltekit-flash-message/server";
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
+  await loadUserData(cookies, params.id);
+
   if (!can(EDIT_DETAILS)) {
     redirect(
       307,
