3a616ec6fee910e006caa15647f01399fe81f950
core <core@coredoes.dev>
2024-03-08 22:54:06 -0500

    request deleting

--- src/routes/[id]/training/requests/+page.server.ts
+++ src/routes/[id]/training/requests/+page.server.ts
@@ -1,7 +1,7 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { loadUserData } from "$lib/auth";
 import { can } from "$lib/perms/can";
-import { TRAIN } from "$lib/perms/permissions";
+import { DELETE_REQUEST, TRAIN } from "$lib/perms/permissions";
 import { redirect } from "sveltekit-flash-message/server";
 import prisma from "$lib/prisma";
 
@@ -57,5 +57,21 @@ export const load: PageServerLoad = async ({cookies, params}) => {
     unassigned,
     assigned,
     expired
+  }
+}
+
+export const actions: Actions = {
+  deleteRequest: async (event) => {
+    await loadUserData(event.cookies, event.params.id);
+
+    if (!can(DELETE_REQUEST)) {
+      redirect(307, `/${event.params.id}/training`, {type: 'error', message: 'You don\'t have permission to do that.'}, event.cookies);
+    }
+
+    await prisma.trainingRequest.delete({
+      where: {
+        id: (await event.request.formData()).get("id")?.toString() || ""
+      }
+    })
   }
 }
