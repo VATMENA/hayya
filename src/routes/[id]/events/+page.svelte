fb9fea83e8109a594a6a4284b1094f0983fdd753
Keelan Daye <keelan.daye@gmail.com>
2024-04-05 01:24:53 +0100

    add event cards

--- src/routes/[id]/events/+page.svelte
+++ src/routes/[id]/events/+page.svelte
@@ -22,6 +22,7 @@
   import DataTableBanner from "./data-table-banner.svelte";
   import DataTableActions from "./data-table-actions.svelte";
   import { MANAGE_EVENTS } from "$lib/perms/permissions";
+  import EventCard from "./event-card.svelte";
 
   export let data: PageData;
 
@@ -93,38 +94,48 @@
   {/if}
 </div>
 
-<Table.Root {...$tableAttrs}>
-  <Table.Header>
-    {#each $headerRows as headerRow}
-      <Subscribe rowAttrs={headerRow.attrs()}>
-        <Table.Row>
-          {#each headerRow.cells as cell (cell.id)}
-            <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
-              <Table.Head {...attrs}>
-                <Render of={cell.render()} />
-              </Table.Head>
-            </Subscribe>
-          {/each}
-        </Table.Row>
-      </Subscribe>
-    {/each}
-  </Table.Header>
-  <Table.Body {...$tableBodyAttrs}>
-    {#each $pageRows as row (row.id)}
-      <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-        <Table.Row {...rowAttrs}>
-          {#each row.cells as cell (cell.id)}
-            <Subscribe attrs={cell.attrs()} let:attrs>
-              <Table.Cell {...attrs}>
-                <Render of={cell.render()} />
-              </Table.Cell>
-            </Subscribe>
-          {/each}
-        </Table.Row>
-      </Subscribe>
-    {/each}
-  </Table.Body>
-</Table.Root>
+<div class="flex flex-row flex-wrap">
+  {#each data.events as event (event.id)}
+    {#if event.public}
+      <EventCard {event} />
+    {/if}
+  {/each}
+</div>
+
+{#if canManageEvents}
+  <Table.Root {...$tableAttrs}>
+    <Table.Header>
+      {#each $headerRows as headerRow}
+        <Subscribe rowAttrs={headerRow.attrs()}>
+          <Table.Row>
+            {#each headerRow.cells as cell (cell.id)}
+              <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
+                <Table.Head {...attrs}>
+                  <Render of={cell.render()} />
+                </Table.Head>
+              </Subscribe>
+            {/each}
+          </Table.Row>
+        </Subscribe>
+      {/each}
+    </Table.Header>
+    <Table.Body {...$tableBodyAttrs}>
+      {#each $pageRows as row (row.id)}
+        <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+          <Table.Row {...rowAttrs}>
+            {#each row.cells as cell (cell.id)}
+              <Subscribe attrs={cell.attrs()} let:attrs>
+                <Table.Cell {...attrs}>
+                  <Render of={cell.render()} />
+                </Table.Cell>
+              </Subscribe>
+            {/each}
+          </Table.Row>
+        </Subscribe>
+      {/each}
+    </Table.Body>
+  </Table.Root>
+{/if}
 
 <!--
 <Table.Root>
