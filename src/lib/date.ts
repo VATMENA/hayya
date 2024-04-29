3dfdcee77efdf91b6b87d8dc351027071d0b0595
Keelan Daye <keelan.daye@gmail.com>
2024-04-29 20:25:03 +0100

    add maximum unit to relativeTime function

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -25,12 +25,23 @@ export function humanReadableDateOnly(d: Date): string {
   return new Intl.DateTimeFormat("en-US", options).format(d);
 }
 
-export function relativeTime(fromDate: Date, toDate: Date = new Date()) {
+
+/**
+ * Calculates the relative time between two dates.
+ *
+ * @param {Date} fromDate - The starting date.
+ * @param {Date} [toDate=new Date()] - The ending date. Defaults to the current date.
+ * @param {("second" | "minute" | "hour" | "day" | "month" | "year")} [maximumUnit] - The maximum unit of time to display. Defaults to years.
+ * @return {string} The relative time between the two dates. Returns "Unknown" if the difference is beyond the maximum unit.
+ */
+export function relativeTime(fromDate: Date, toDate: Date = new Date(), maximumUnit?: "second" | "minute" | "hour" | "day" | "month" | "year") {
   const units = [
-    { max: 1000 * 60, name: "second", divisor: 1000 },
-    { max: 1000 * 60 * 60, name: "minute", divisor: 1000 * 60 },
-    { max: 1000 * 60 * 60 * 24, name: "hour", divisor: 1000 * 60 * 60 },
-    { max: Infinity, name: "day", divisor: 1000 * 60 * 60 * 24 },
+    { max: maximumUnit === "second" ? Infinity : 1000 * 60, name: "second", divisor: 1000 },
+    { max: maximumUnit === "minute" ? Infinity : 1000 * 60 * 60, name: "minute", divisor: 1000 * 60 },
+    { max: maximumUnit === "hour" ? Infinity : 1000 * 60 * 60 * 24, name: "hour", divisor: 1000 * 60 * 60 },
+    { max: maximumUnit === "day" ? Infinity : 1000 * 60 * 60 * 24 * 30, name: "day", divisor: 1000 * 60 * 60 * 24 },
+    { max: maximumUnit === "month" ? Infinity : 1000 * 60 * 60 * 24 * 30 * 12, name: "month", divisor: 1000 * 60 * 60 * 24 * 30 },
+    { max: Infinity, name: "year", divisor: 1000 * 60 * 60 * 24 * 365 },
   ];
 
   const millis = fromDate.getTime() - toDate.getTime();
